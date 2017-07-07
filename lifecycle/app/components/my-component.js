933e3d3fd3a5fc261b5c18fe6d19398432ea6305
Saravanan Rajaraman <rsaravanan.btech@gmail.com>
2017-07-07 10:43:17 +0000

    life cycle tryout

---
+++ lifecycle/app/components/my-component.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  didInitAttrs(options) {
+    console.log('didInitAttrs', options);
+  },
+
+  didUpdateAttrs(options) {
+    console.log('didUpdateAttrs', options);
+  },
+
+  willUpdate(options) {
+    console.log('willUpdate', options);
+  },
+
+  didReceiveAttrs(options) {
+    console.log('didReceiveAttrs', options);
+  },
+
+  willRender() {
+    console.log('willRender');
+  },
+
+  didRender() {
+    console.log('didRender');
+  },
+
+  didInsertElement() {
+    console.log('didInsertElement');
+  },
+
+  didUpdate(options) {
+    console.log('didUpdate', options);
+  },
+});
